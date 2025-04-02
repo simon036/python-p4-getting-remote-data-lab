ecf19478ed9d7bfe3f5a8d7032bf2f06acc83a09
Simon <ndungusimon564@gmail.com>
2025-04-02 10:27:10 +0300

    complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,11 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
+        response = requests.get(self.url)
+        return response.content
+
         pass
 
     def load_json(self):
+        return json.loads(self.get_response_body())
         pass
